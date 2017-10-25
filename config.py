edf2544c4aeee338976f5b7bf8609c08acb59b68
kageru <8497max@gmail.com>
2017-10-25 14:22:23 +0200

    add port selection

--- config.py
+++ config.py
@@ -18,10 +18,13 @@ custom_curl_command = None
 
 # This is where the screenshots are saved locally
 local_directory = '/home/kageru/pyshare/'
+
 # Directory on the ftp server where you want the screenshots to be sent to
 remote_directory = '/usr/share/nginx/html/pyshare/'
 # Template for the link that the script will generate. {} is the filename
 url_template = 'https://your_domain.com/pyshare/{}'
+remote_port = 5000
+
 # In case you want a certain prefix for all files. Can also be an empty string
 prefix = ''
 # Number of random characters in the filename
--- pyshare_receiver.py
+++ pyshare_receiver.py
@@ -67,4 +67,4 @@ def receive_file() -> tuple:
 
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run(ssl_context='adhoc', port=config.remote_port)
