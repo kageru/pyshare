b07a4b79579c8816a73fe41617e1803209e74f12
kageru <kageru@encode.moe>
2017-11-20 15:12:02 +0100

    started undoing bad ideas

--- config.py
+++ config.py
@@ -3,25 +3,21 @@ uploader = 'sftp'
 
 # (S)FTP credentials, only if you want to use SFTP for uploads
 # Set all to None that you don’t need
-sftp_address = ''
+sftp_address = 'your_domain.com'
+sftp_port = 22
 username = None
 password = None
 private_key = None
-
-# curl parameters, if you're using pyshare_receivere
-curl_target = 'your_domain.com:5000'
-curl_user = 'user1'
-curl_password = 'password'
+private_key_pass = None
 
 # This should contain a complete curl command with a {} to insert the filename.
-# Setting this to anything but None will result in the other curl parameters to be ignored
 # Example: 'curl -F"file=@{}" https://0x0.st'
-custom_curl_command = None
+curl_command = None
 
 # This is where the screenshots are saved locally
 local_directory = '/home/kageru/pyshare/'
 
-# Directory on the ftp server where you want the screenshots to be sent to
+# Directory on the ftp server where you want the screenshots to be sent
 remote_directory = '/usr/share/nginx/html/pyshare/'
 # Template for the link that the script will generate. {} is the filename
 url_template = 'https://your_domain.com/pyshare/{}'
